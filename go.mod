43a5aa1bf236ca8e9e2c708c4724ac163149360f
Zack Stevens <zack.st7@gmail.com>
2022-05-11 09:43:01 -0600

    fix: Add persistence and better handling of offsets and dates to ensure we don't miss or duplicate data (#4)

--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.18
 
 require (
 	github.com/catalystsquad/app-utils-go v1.0.4
+	github.com/dgraph-io/badger/v3 v3.2103.2
 	github.com/go-playground/validator/v10 v10.11.0
 	github.com/joomcode/errorx v1.1.0
-	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.4.0
 	github.com/spf13/viper v1.11.0
 	github.com/tidwall/gjson v1.14.1
@@ -15,9 +15,19 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dgraph-io/ristretto v0.1.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/flatbuffers v1.12.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
@@ -28,6 +38,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rs/zerolog v1.26.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -36,9 +47,12 @@ require (
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- pkg/lightning_poller.go
+++ pkg/lightning_poller.go
@@ -3,17 +3,28 @@ package pkg
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/catalystsquad/app-utils-go/errorutils"
 	"github.com/catalystsquad/app-utils-go/logging"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/go-playground/validator/v10"
 	"github.com/joomcode/errorx"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/tidwall/gjson"
 	"github.com/valyala/fasthttp"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
+
+var zeroTime = time.Time{}
+
+type Position struct {
+	Offset           int
+	LastModifiedDate *time.Time
+}
 
 // salesforceCredentials represents the response from salesforce's /services/oauth2/token endpoint to get an access token
 type salesforceCredentials struct {
@@ -28,24 +39,29 @@ type salesforceCredentials struct {
 type LightningPoller struct {
 	config  *RunConfig
 	polling bool
+	db      *badger.DB
 }
 
 type RunConfig struct {
-	Domain       string              `json:"domain" validate:"required"`
-	ClientId     string              `json:"client_id" validate:"required"`
-	ClientSecret string              `json:"client_secret" validate:"required"`
-	Username     string              `json:"username" validate:"required"`
-	Password     string              `json:"password" validate:"required"`
-	GrantType    string              `json:"grant_type" validate:"required"`
-	ApiVersion   string              `json:"api_version" validate:"required"`
-	Queries      []QueryWithCallback `validate:"required"`
-	Ticker       *time.Ticker
-	AccessToken  string `json:"access_token"`
+	Domain             string              `json:"domain" validate:"required"`
+	ClientId           string              `json:"client_id" validate:"required"`
+	ClientSecret       string              `json:"client_secret" validate:"required"`
+	Username           string              `json:"username" validate:"required"`
+	Password           string              `json:"password" validate:"required"`
+	GrantType          string              `json:"grant_type" validate:"required"`
+	ApiVersion         string              `json:"api_version" validate:"required"`
+	Queries            []QueryWithCallback `validate:"required"`
+	Ticker             *time.Ticker
+	AccessToken        string `json:"access_token"`
+	PersistenceEnabled bool   `json:"persistence_enabled"`
+	PersistencePath    string `json:"persistence_path"`
+	Limit              int    `json:"limit" validate:"gte=1,lte=1000"` // limit must be between 1-1000
 }
 
 type QueryWithCallback struct {
-	Query    func() string                  `json:"query" validate:"required"`
-	Callback func(result []byte, err error) `validate:"required"`
+	Query          func() string                  `json:"query" validate:"required"`
+	PersistenceKey string                         `json:"persistenceKey"`
+	Callback       func(result []byte, err error) `validate:"required"`
 }
 
 func NewLightningPoller(queries []QueryWithCallback) (*LightningPoller, error) {
@@ -56,6 +72,13 @@ func NewLightningPoller(queries []QueryWithCallback) (*LightningPoller, error) {
 }
 
 func (p *LightningPoller) Run() {
+	if p.config.PersistenceEnabled {
+		err := p.openBadgerDb(p.config.PersistencePath)
+		if err != nil {
+			return
+		}
+	}
+	defer p.closeBadgerDb()
 	for range p.config.Ticker.C {
 		p.poll()
 	}
@@ -67,14 +90,65 @@ func (p *LightningPoller) poll() {
 		defer func() { p.polling = false }()
 		logging.Log.Debug("polling")
 		for _, queryWithCallback := range p.config.Queries {
-			query := queryWithCallback.Query()
+			query, err := p.getPollQuery(queryWithCallback)
+			if err != nil {
+				errorutils.LogOnErr(nil, "error building query", err)
+				continue
+			}
+			logging.Log.WithFields(logrus.Fields{"query": query}).Debug("query")
 			result, err := p.queryWithAuth(query)
+			if err != nil {
+				errorutils.LogOnErr(nil, "error making soql query", err)
+			} else {
+				// if there is no error, update last modified
+				if err == nil {
+					newPosition, newPositionErr := getNewPositionFromResult(result)
+					if newPositionErr != nil {
+						errorutils.LogOnErr(logging.Log.WithField("query", query), "error parsing LastModifiedDate from result", err)
+						continue
+					}
+					if newPosition.LastModifiedDate != nil {
+						err = p.setPosition(queryWithCallback.PersistenceKey, newPosition)
+						if err != nil {
+							errorutils.LogOnErr(logging.Log.WithField("query", query), "error updating last modified date", err)
+							continue
+						}
+					}
+				}
+			}
 			queryWithCallback.Callback(result, err)
 		}
 		logging.Log.Debug("polling complete")
 	} else {
 		logging.Log.Debug("not polling because poll is currently in progress")
 	}
+}
+
+func getNewPositionFromResult(result []byte) (position Position, err error) {
+	numRecords := gjson.GetBytes(result, "records.#").Int()
+	finalArrayIndex := numRecords - 1
+	if numRecords > 0 {
+		path := fmt.Sprintf("records.%d.LastModifiedDate", finalArrayIndex)
+		finalLastModifiedDateResult := gjson.GetBytes(result, path)
+		finalLastModifiedDateString := finalLastModifiedDateResult.String()
+		// loop backwards until we hit a different date, incrementing the offset each time.
+		for i := finalArrayIndex; i >= 0; i-- {
+			// break if the date is different
+			path := fmt.Sprintf("records.%d.LastModifiedDate", i)
+			if gjson.GetBytes(result, path).String() != finalLastModifiedDateString {
+				break
+			}
+			// date is the same, increment offset
+			position.Offset++
+		}
+		timestamp, timestampErr := getTimestampFromResultLastModifiedDate(finalLastModifiedDateString)
+		if timestampErr != nil {
+			err = timestampErr
+			return
+		}
+		position.LastModifiedDate = &timestamp
+	}
+	return
 }
 
 func (p *LightningPoller) queryWithAuth(query string) ([]byte, error) {
@@ -116,9 +190,11 @@ func (p *LightningPoller) queryWithAuth(query string) ([]byte, error) {
 }
 
 func (p *LightningPoller) query(query string) ([]byte, int, error) {
+	logging.Log.WithFields(logrus.Fields{"query": query}).Debug("sending soql query")
 	req := fasthttp.AcquireRequest()
 	defer fasthttp.ReleaseRequest(req)
-	req.SetRequestURI(p.getQueryUrl(query))
+	uri := p.getQueryUrl(query)
+	req.SetRequestURI(uri)
 	p.addAuthHeader(req)
 	res := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseResponse(res)
@@ -180,19 +256,23 @@ func initConfig(queries []QueryWithCallback) (*RunConfig, error) {
 	viper.AutomaticEnv() // read in environment variables that match
 	viper.SetDefault("grant_type", "password")
 	viper.SetDefault("poll_interval", "10s")
+	viper.SetDefault("persistence_enabled", false)
+	viper.SetDefault("persistence_path", ".")
 	viper.SetDefault("api_version", "54.0")
 	config := &RunConfig{
-		Domain:       viper.GetString("domain"),
-		ClientId:     viper.GetString("client_id"),
-		ClientSecret: viper.GetString("client_secret"),
-		Username:     viper.GetString("username"),
-		Password:     viper.GetString("password"),
-		GrantType:    viper.GetString("grant_type"),
-		ApiVersion:   viper.GetString("api_version"),
-		Queries:      queries,
-		Ticker:       time.NewTicker(viper.GetDuration("poll_interval")),
+		Domain:             viper.GetString("domain"),
+		ClientId:           viper.GetString("client_id"),
+		ClientSecret:       viper.GetString("client_secret"),
+		Username:           viper.GetString("username"),
+		Password:           viper.GetString("password"),
+		GrantType:          viper.GetString("grant_type"),
+		ApiVersion:         viper.GetString("api_version"),
+		Queries:            queries,
+		Ticker:             time.NewTicker(viper.GetDuration("poll_interval")),
+		PersistenceEnabled: viper.GetBool("persistence_enabled"),
+		PersistencePath:    viper.GetString("persistence_path"),
+		Limit:              viper.GetInt("limit"),
 	}
-
 	theValidator := validator.New()
 	err := theValidator.Struct(config)
 	if err != nil {
@@ -224,4 +304,88 @@ func (p *LightningPoller) getBaseUrl() string {
 // addAuthHeader adds the access token from the config to the request
 func (p *LightningPoller) addAuthHeader(req *fasthttp.Request) {
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", p.config.AccessToken))
+}
+
+func (p *LightningPoller) openBadgerDb(path string) error {
+	db, err := badger.Open(badger.DefaultOptions(path))
+	if err != nil {
+		errorutils.LogOnErr(logging.Log.WithField("path", path), "error opening badger db", err)
+	} else {
+		p.db = db
+	}
+	return err
+}
+
+func (p *LightningPoller) closeBadgerDb() {
+	err := p.db.Close()
+	errorutils.LogOnErr(nil, "error closing badger database", err)
+}
+
+// getPollQuery is used to modify the base query according to configuration.
+func (p *LightningPoller) getPollQuery(queryWithCallback QueryWithCallback) (string, error) {
+	var builder strings.Builder
+	builder.WriteString(queryWithCallback.Query())
+	// if persistence is disabled just return the query as is
+	if !p.config.PersistenceEnabled {
+		return builder.String(), nil
+	} else {
+		// query for last updated and update query based on stored timestamp
+		persistenceKey := queryWithCallback.PersistenceKey
+		position, err := p.getPosition([]byte(persistenceKey))
+		if err != nil && !strings.Contains("Key not found", err.Error()) {
+			return "", err
+		}
+		operator := "where"
+		// if there's a where clause, switch the operator to and so we append a condition instead of creating one
+		if strings.Contains(strings.ToLower(builder.String()), operator) {
+			operator = "and"
+		}
+		// use of rfc3339 is important here. SOQL uses + to indicate a space, so it parses out timestamp with + in them as a space, which is an invalid timestamp
+		// and then it gets mad that the datetime isn't valid because it made it invalid by replacing the + (for the timezone) with a space.
+		// if the time is not zero, use time - 2 seconds to make sure we never catch mid second updates
+		if position.LastModifiedDate.UTC() != zeroTime.UTC() {
+			correctedTime := position.LastModifiedDate.Add(-2 * time.Second)
+			position.LastModifiedDate = &correctedTime
+		}
+		dateTimeString := getRfcFormattedUtcTimestampString(*position.LastModifiedDate)
+		builder.WriteString(fmt.Sprintf(" %s LastModifiedDate >= %s order by LastModifiedDate, Id limit %d offset %d", operator, dateTimeString, p.config.Limit, position.Offset))
+		return builder.String(), nil
+	}
+}
+
+// getPosition fetches the persisted position. If there is none, then it initializes to zero values
+func (p *LightningPoller) getPosition(key []byte) (position Position, err error) {
+	err = p.db.View(func(txn *badger.Txn) error {
+		item, getErr := txn.Get(key)
+		if getErr != nil {
+			return getErr
+		}
+		return item.Value(func(val []byte) error {
+			return json.Unmarshal(val, &position)
+		})
+	})
+	// if there is no stored date, use a zero date
+	if position.LastModifiedDate == nil {
+		position.LastModifiedDate = &time.Time{}
+	}
+	return
+}
+
+func getRfcFormattedUtcTimestampString(timestamp time.Time) string {
+	return timestamp.UTC().Format(time.RFC3339)
+}
+
+func (p *LightningPoller) setPosition(key string, position Position) error {
+	positionBytes, err := json.Marshal(position)
+	if err != nil {
+		return err
+	}
+	err = p.db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(key), positionBytes)
+	})
+	return err
+}
+
+func getTimestampFromResultLastModifiedDate(lastModifiedDate string) (timestamp time.Time, err error) {
+	return time.Parse("2006-01-02T15:04:05.000+0000", lastModifiedDate)
 }
