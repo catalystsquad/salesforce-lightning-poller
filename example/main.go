bf4b671ffc2d36ade768892eb3fba1a2263b165a
Zack Stevens <zack.st7@gmail.com>
2022-06-07 10:17:30 -0600

    fix: Fix position tracking edge case (#5)

--- example/main.go
+++ example/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/catalystsquad/app-utils-go/errorutils"
 	"github.com/catalystsquad/app-utils-go/logging"
 	"github.com/catalystsquad/salesforce-lightning-poller/pkg"
+	pkg2 "github.com/catalystsquad/salesforce-utils/pkg"
 	"github.com/sirupsen/logrus"
 )
 
@@ -14,12 +15,13 @@ func main() {
 				return "select fields(all) from Property__c"
 			},
 			PersistenceKey: "property__c",
-			Callback: func(result []byte, err error) {
+			Callback: func(result []byte, err error) bool {
 				logging.Log.WithFields(logrus.Fields{"result": string(result)}).Info("query callback")
+				return true
 			},
 		},
 	}
-	poller, err := pkg.NewLightningPoller(queries)
+	poller, err := pkg.NewLightningPoller(queries, pkg2.Config{})
 	errorutils.PanicOnErr(nil, "error creating poller", err)
 	poller.Run()
 }
--- pkg/lightning_poller.go
+++ pkg/lightning_poller.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/catalystsquad/app-utils-go/errorutils"
 	"github.com/catalystsquad/app-utils-go/logging"
+	"github.com/catalystsquad/salesforce-utils/pkg"
 	"github.com/dgraph-io/badger/v3"
 	"github.com/go-playground/validator/v10"
 	"github.com/joomcode/errorx"
@@ -12,62 +13,49 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/tidwall/gjson"
-	"github.com/valyala/fasthttp"
-	"net/http"
 	"os"
 	"strings"
 	"time"
 )
-
-var zeroTime = time.Time{}
 
 type Position struct {
 	Offset           int
 	LastModifiedDate *time.Time
 }
 
-// salesforceCredentials represents the response from salesforce's /services/oauth2/token endpoint to get an access token
-type salesforceCredentials struct {
-	AccessToken string `json:"access_token"`
-	InstanceUrl string `json:"instance_url"`
-	Id          string `json:"id"`
-	TokenType   string `json:"token_type"`
-	IssuedAt    int    `json:"issued_at,string"`
-	Signature   string `json:"signature"`
-}
-
 type LightningPoller struct {
-	config  *RunConfig
-	polling bool
-	db      *badger.DB
+	config    *RunConfig
+	polling   bool
+	db        *badger.DB
+	sfUtils   *pkg.SalesforceUtils
+	positions map[string]*Position
 }
 
 type RunConfig struct {
-	Domain             string              `json:"domain" validate:"required"`
-	ClientId           string              `json:"client_id" validate:"required"`
-	ClientSecret       string              `json:"client_secret" validate:"required"`
-	Username           string              `json:"username" validate:"required"`
-	Password           string              `json:"password" validate:"required"`
-	GrantType          string              `json:"grant_type" validate:"required"`
-	ApiVersion         string              `json:"api_version" validate:"required"`
 	Queries            []QueryWithCallback `validate:"required"`
 	Ticker             *time.Ticker
-	AccessToken        string `json:"access_token"`
 	PersistenceEnabled bool   `json:"persistence_enabled"`
 	PersistencePath    string `json:"persistence_path"`
 	Limit              int    `json:"limit" validate:"gte=1,lte=1000"` // limit must be between 1-1000
 }
 
 type QueryWithCallback struct {
-	Query          func() string                  `json:"query" validate:"required"`
-	PersistenceKey string                         `json:"persistenceKey"`
-	Callback       func(result []byte, err error) `validate:"required"`
+	Query          func() string                       `json:"query" validate:"required"`
+	PersistenceKey string                              `json:"persistenceKey"`
+	Callback       func(result []byte, err error) bool `validate:"required"`
 }
 
-func NewLightningPoller(queries []QueryWithCallback) (*LightningPoller, error) {
+func NewLightningPoller(queries []QueryWithCallback, sfConfig pkg.Config) (*LightningPoller, error) {
 	poller := &LightningPoller{}
 	config, err := initConfig(queries)
+	if err != nil {
+		return nil, err
+	}
 	poller.config = config
+	poller.sfUtils, err = pkg.NewSalesforceUtils(true, sfConfig)
+	if err != nil {
+		return nil, err
+	}
 	return poller, err
 }
 
@@ -79,9 +67,33 @@ func (p *LightningPoller) Run() {
 		}
 	}
 	defer p.closeBadgerDb()
+	err := p.loadPositions()
+	errorutils.PanicOnErr(nil, "error loading poller position", err)
 	for range p.config.Ticker.C {
 		p.poll()
 	}
+}
+
+// loadPositions loads positions into memory, using saved state if saved state exists
+func (p *LightningPoller) loadPositions() error {
+	// init poller's positions map
+	p.positions = map[string]*Position{}
+	// load position for each query based on perisstence key
+	for _, query := range p.config.Queries {
+		key := query.PersistenceKey
+		if p.config.PersistenceEnabled {
+			// fetch saved position and set it on the map
+			savedPosition, err := p.getPosition([]byte(key))
+			if err != nil {
+				return err
+			}
+			p.positions[key] = savedPosition
+		} else {
+			// persistence is disabled, initialize to zero values
+			p.positions[key] = &Position{LastModifiedDate: &time.Time{}}
+		}
+	}
+	return nil
 }
 
 func (p *LightningPoller) poll() {
@@ -96,27 +108,25 @@ func (p *LightningPoller) poll() {
 				continue
 			}
 			logging.Log.WithFields(logrus.Fields{"query": query}).Debug("query")
-			result, err := p.queryWithAuth(query)
+			response, err := p.sfUtils.ExecuteSoqlQuery(query)
 			if err != nil {
 				errorutils.LogOnErr(nil, "error making soql query", err)
 			} else {
-				// if there is no error, update last modified
-				if err == nil {
-					newPosition, newPositionErr := getNewPositionFromResult(result)
-					if newPositionErr != nil {
-						errorutils.LogOnErr(logging.Log.WithField("query", query), "error parsing LastModifiedDate from result", err)
-						continue
-					}
-					if newPosition.LastModifiedDate != nil {
-						err = p.setPosition(queryWithCallback.PersistenceKey, newPosition)
-						if err != nil {
-							errorutils.LogOnErr(logging.Log.WithField("query", query), "error updating last modified date", err)
-							continue
+				if len(response.Records) > 0 {
+					recordsJson, jsonErr := json.Marshal(response.Records)
+					if jsonErr != nil {
+						errorutils.LogOnErr(nil, "error making soql query", err)
+					} else {
+						savePosition := queryWithCallback.Callback(recordsJson, err)
+						if savePosition {
+							positionErr := p.updatePosition(queryWithCallback.PersistenceKey, recordsJson)
+							if positionErr != nil {
+								errorutils.LogOnErr(nil, "error updating position", positionErr)
+							}
 						}
 					}
 				}
 			}
-			queryWithCallback.Callback(result, err)
 		}
 		logging.Log.Debug("polling complete")
 	} else {
@@ -124,111 +134,55 @@ func (p *LightningPoller) poll() {
 	}
 }
 
-func getNewPositionFromResult(result []byte) (position Position, err error) {
-	numRecords := gjson.GetBytes(result, "records.#").Int()
-	finalArrayIndex := numRecords - 1
-	if numRecords > 0 {
-		path := fmt.Sprintf("records.%d.LastModifiedDate", finalArrayIndex)
-		finalLastModifiedDateResult := gjson.GetBytes(result, path)
-		finalLastModifiedDateString := finalLastModifiedDateResult.String()
-		// loop backwards until we hit a different date, incrementing the offset each time.
-		for i := finalArrayIndex; i >= 0; i-- {
-			// break if the date is different
-			path := fmt.Sprintf("records.%d.LastModifiedDate", i)
-			if gjson.GetBytes(result, path).String() != finalLastModifiedDateString {
-				break
-			}
-			// date is the same, increment offset
-			position.Offset++
-		}
-		timestamp, timestampErr := getTimestampFromResultLastModifiedDate(finalLastModifiedDateString)
-		if timestampErr != nil {
-			err = timestampErr
-			return
-		}
-		position.LastModifiedDate = &timestamp
-	}
-	return
-}
-
-func (p *LightningPoller) queryWithAuth(query string) ([]byte, error) {
-	// check for empty access token, attempt to get token if it's empty
-	if p.config.AccessToken == "" {
-		err := p.getSalesforceCredentials()
-		if err != nil {
-			return nil, err
-		}
-	}
-	// make query
-	body, statusCode, err := p.query(query)
-	// return on error
+func (p *LightningPoller) updatePosition(key string, recordsJson []byte) error {
+	newPosition, err := getNewPositionFromResult(recordsJson)
 	if err != nil {
-		return nil, errorx.Decorate(err, "error making query")
+		return err
 	}
-
-	if statusCode == http.StatusOK {
-		// return the body if we got a 200
-		return body, nil
-	} else if statusCode == http.StatusUnauthorized {
-		// token is invalid or expired, authenticate and try again
-		err = p.getSalesforceCredentials()
-		body, statusCode, err = p.query(query)
-		// return on error
-		if err != nil {
-			return nil, errorx.Decorate(err, "error making query")
-		}
-		if statusCode == http.StatusOK {
-			return body, nil
-		} else {
-			// return an error
-			return nil, errorx.Decorate(err, "unexpected status code: %d with body: %s", statusCode, body)
-		}
+	position := p.positions[key]
+	// if the date is the same, add the offsets. This would happen if you got several pages with the same date. If you
+	/// don't combine the offsets then you'll just get the same page of data over and over forever.
+	if position.LastModifiedDate == newPosition.LastModifiedDate {
+		position.Offset += newPosition.Offset
 	} else {
-		// return an error
-		return nil, errorx.Decorate(err, "unexpected status code: %d with body: %s", statusCode, body)
+		// date is different, replace the position entirely
+		position.LastModifiedDate = newPosition.LastModifiedDate
+		position.Offset = newPosition.Offset
 	}
-}
-
-func (p *LightningPoller) query(query string) ([]byte, int, error) {
-	logging.Log.WithFields(logrus.Fields{"query": query}).Debug("sending soql query")
-	req := fasthttp.AcquireRequest()
-	defer fasthttp.ReleaseRequest(req)
-	uri := p.getQueryUrl(query)
-	req.SetRequestURI(uri)
-	p.addAuthHeader(req)
-	res := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseResponse(res)
-	err := fasthttp.Do(req, res)
-	return res.Body(), res.StatusCode(), err
-}
-
-func (p *LightningPoller) getSalesforceCredentials() error {
-	body, statusCode, err := p.getSalesforceAccessToken()
-	if err != nil {
-		return errorx.Decorate(err, "error getting access token")
+	// update saved position if persistence is enabled
+	if p.config.PersistenceEnabled {
+		err := p.setPosition(key, *position)
+		if err != nil {
+			return err
+		}
 	}
-	if statusCode != 200 {
-		return errorx.Decorate(err, "error getting access token")
-	}
-	var creds salesforceCredentials
-	err = json.Unmarshal(body, &creds)
-	if err != nil {
-		return errorx.Decorate(err, "error getting access token")
-	}
-	p.config.AccessToken = creds.AccessToken
+	logging.Log.WithFields(logrus.Fields{"lastModifiedDate": position.LastModifiedDate, "offset": position.Offset}).Debug("updated position")
 	return nil
 }
 
-func (p *LightningPoller) getSalesforceAccessToken() ([]byte, int, error) {
-	req := fasthttp.AcquireRequest()
-	defer fasthttp.ReleaseRequest(req)
-	uri := p.getAuthUrl()
-	req.SetRequestURI(uri)
-	req.Header.SetMethod("POST")
-	res := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseResponse(res)
-	err := fasthttp.Do(req, res)
-	return res.Body(), res.StatusCode(), err
+func getNewPositionFromResult(recordsJson []byte) (position Position, err error) {
+	numRecords := gjson.GetBytes(recordsJson, "#").Int()
+	finalArrayIndex := numRecords - 1
+	path := fmt.Sprintf("%d.LastModifiedDate", finalArrayIndex)
+	finalLastModifiedDateResult := gjson.GetBytes(recordsJson, path)
+	finalLastModifiedDateString := finalLastModifiedDateResult.String()
+	// loop backwards until we hit a different date, incrementing the offset each time.
+	for i := finalArrayIndex; i >= 0; i-- {
+		// break if the date is different
+		path := fmt.Sprintf("%d.LastModifiedDate", i)
+		if gjson.GetBytes(recordsJson, path).String() != finalLastModifiedDateString {
+			break
+		}
+		// date is the same, increment offset
+		position.Offset++
+	}
+	timestamp, timestampErr := getTimestampFromResultLastModifiedDate(finalLastModifiedDateString)
+	if timestampErr != nil {
+		err = timestampErr
+		return
+	}
+	position.LastModifiedDate = &timestamp
+	return
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -260,13 +214,6 @@ func initConfig(queries []QueryWithCallback) (*RunConfig, error) {
 	viper.SetDefault("persistence_path", ".")
 	viper.SetDefault("api_version", "54.0")
 	config := &RunConfig{
-		Domain:             viper.GetString("domain"),
-		ClientId:           viper.GetString("client_id"),
-		ClientSecret:       viper.GetString("client_secret"),
-		Username:           viper.GetString("username"),
-		Password:           viper.GetString("password"),
-		GrantType:          viper.GetString("grant_type"),
-		ApiVersion:         viper.GetString("api_version"),
 		Queries:            queries,
 		Ticker:             time.NewTicker(viper.GetDuration("poll_interval")),
 		PersistenceEnabled: viper.GetBool("persistence_enabled"),
@@ -283,27 +230,6 @@ func initConfig(queries []QueryWithCallback) (*RunConfig, error) {
 		return nil, errorx.DecorateMany("error initializing config", errs...)
 	}
 	return config, nil
-}
-
-// getQueryUrl gets a formatted url to the soql query endpoint
-func (p *LightningPoller) getQueryUrl(query string) string {
-	formattedQuery := strings.Replace(query, " ", "+", -1)
-	return fmt.Sprintf("%s/services/data/v%s/query?q=%s", p.getBaseUrl(), p.config.ApiVersion, formattedQuery)
-}
-
-// getAuthUrl gets a formatted url to the token endpoint
-func (p *LightningPoller) getAuthUrl() string {
-	return fmt.Sprintf("%s/services/oauth2/token?client_id=%s&client_secret=%s&username=%s&password=%s&grant_type=%s", p.getBaseUrl(), p.config.ClientId, p.config.ClientSecret, p.config.Username, p.config.Password, p.config.GrantType)
-}
-
-// getBaseUrl gets a base url using the configured domain
-func (p *LightningPoller) getBaseUrl() string {
-	return fmt.Sprintf("https://%s", p.config.Domain)
-}
-
-// addAuthHeader adds the access token from the config to the request
-func (p *LightningPoller) addAuthHeader(req *fasthttp.Request) {
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", p.config.AccessToken))
 }
 
 func (p *LightningPoller) openBadgerDb(path string) error {
@@ -325,36 +251,28 @@ func (p *LightningPoller) closeBadgerDb() {
 func (p *LightningPoller) getPollQuery(queryWithCallback QueryWithCallback) (string, error) {
 	var builder strings.Builder
 	builder.WriteString(queryWithCallback.Query())
-	// if persistence is disabled just return the query as is
-	if !p.config.PersistenceEnabled {
-		return builder.String(), nil
-	} else {
-		// query for last updated and update query based on stored timestamp
-		persistenceKey := queryWithCallback.PersistenceKey
-		position, err := p.getPosition([]byte(persistenceKey))
-		if err != nil && !strings.Contains("Key not found", err.Error()) {
-			return "", err
-		}
-		operator := "where"
-		// if there's a where clause, switch the operator to and so we append a condition instead of creating one
-		if strings.Contains(strings.ToLower(builder.String()), operator) {
-			operator = "and"
-		}
-		// use of rfc3339 is important here. SOQL uses + to indicate a space, so it parses out timestamp with + in them as a space, which is an invalid timestamp
-		// and then it gets mad that the datetime isn't valid because it made it invalid by replacing the + (for the timezone) with a space.
-		// if the time is not zero, use time - 2 seconds to make sure we never catch mid second updates
-		if position.LastModifiedDate.UTC() != zeroTime.UTC() {
-			correctedTime := position.LastModifiedDate.Add(-2 * time.Second)
-			position.LastModifiedDate = &correctedTime
-		}
-		dateTimeString := getRfcFormattedUtcTimestampString(*position.LastModifiedDate)
-		builder.WriteString(fmt.Sprintf(" %s LastModifiedDate >= %s order by LastModifiedDate, Id limit %d offset %d", operator, dateTimeString, p.config.Limit, position.Offset))
-		return builder.String(), nil
+	// query for last updated and update query based on stored timestamp
+	persistenceKey := queryWithCallback.PersistenceKey
+	currentPosition := p.positions[persistenceKey]
+	operator := "where"
+	// if there's a where clause, switch the operator to and so we append a condition instead of creating one
+	if strings.Contains(strings.ToLower(builder.String()), operator) {
+		operator = "and"
 	}
+	// use of rfc3339 is important here. SOQL uses + to indicate a space, so it parses out timestamp with + in them as a space, which is an invalid timestamp
+	// and then it gets mad that the datetime isn't valid because it made it invalid by replacing the + (for the timezone) with a space.
+	// if the time is not zero, use time - 2 seconds to make sure we never catch mid second updates
+	//if position.LastModifiedDate.UTC() != zeroTime.UTC() {
+	//	correctedTime := position.LastModifiedDate.Add(-2 * time.Second)
+	//	position.LastModifiedDate = &correctedTime
+	//}
+	dateTimeString := getRfcFormattedUtcTimestampString(*currentPosition.LastModifiedDate)
+	builder.WriteString(fmt.Sprintf(" %s LastModifiedDate >= %s order by LastModifiedDate, Id limit %d offset %d", operator, dateTimeString, p.config.Limit, currentPosition.Offset))
+	return builder.String(), nil
 }
 
 // getPosition fetches the persisted position. If there is none, then it initializes to zero values
-func (p *LightningPoller) getPosition(key []byte) (position Position, err error) {
+func (p *LightningPoller) getPosition(key []byte) (position *Position, err error) {
 	err = p.db.View(func(txn *badger.Txn) error {
 		item, getErr := txn.Get(key)
 		if getErr != nil {
@@ -364,9 +282,12 @@ func (p *LightningPoller) getPosition(key []byte) (position Position, err error)
 			return json.Unmarshal(val, &position)
 		})
 	})
-	// if there is no stored date, use a zero date
-	if position.LastModifiedDate == nil {
-		position.LastModifiedDate = &time.Time{}
+	if err != nil {
+		// if the key is not found, then return a new position with zero state
+		if strings.Contains(err.Error(), "Key not found") {
+			err = nil
+			position = &Position{LastModifiedDate: &time.Time{}}
+		}
 	}
 	return
 }
